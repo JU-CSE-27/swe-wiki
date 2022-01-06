20886c471e36327a0d2cc0b1533fb32f5c49cf9e
Rubaiya Reza Sohana <rubaiyarezasohana1971@gmail.com>
2022-01-06 21:29:30 +0600

    Main Branch

--- ecom/settings.py
+++ ecom/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecom',
+        'USER':'root',
+        'PASSWORD': '',
+        'PORT':'3306',
     }
 }
 
