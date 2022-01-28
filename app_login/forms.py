f1b7731e284d5c0b09c278ca18c06f98ccbd9559
Munia2319 <62173799+Munia2319@users.noreply.github.com>
2022-01-28 16:34:34 +0600

    Update forms.py

--- app_login/forms.py
+++ app_login/forms.py
@@ -1,12 +1,16 @@
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
-"""
-There is a form name createForm
 
 """
-class createUserForm(UserCreationForm):
+There are forms name User-create Form, User-data Edit Form
+
+"""
+
+
+class CreateUserForm(UserCreationForm):
+
 	"""
 	:form name:createUserForm
     :inherit: UserCreationForm
@@ -14,3 +18,15 @@ class createUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+
+
+class EditProfileForm(UserChangeForm):
+
+	"""
+	:form name:UserDataChangeForm
+    :inherit: UserChangeForm
+    """
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'first_name', 'last_name']
