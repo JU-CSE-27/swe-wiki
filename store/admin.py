d485ed869751bf7414d84f0b0734f955d72221c2
Akash <70022481+msaakash@users.noreply.github.com>
2022-01-28 18:30:38 +0600

    Delete store directory

--- store/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
